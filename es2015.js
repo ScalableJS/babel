49b3335ab1360d5995bebae387e4588a1d215cef
Oleg Ch <056@live.ru>
2018-08-18 23:24:20 +0300

    babel

---
+++ es2015.js
@@ -0,0 +1,8 @@
+// npm init
+// npm install -g babel-cli --save-dev
+// npm install babel-preset-es2015 --save-dev
+// babel es2015.js --out-file es5.js
+// babel es2015.js -o es5.js --presets es2015
+const print = massage => console.log(massage);
+
+print('Hi babel!!!')
---
+++ es5.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// npm install babel-cli --save-dev
+// npm install babel-preset-es2015 --save-dev
+// babel es2015.js --out-file es5.js
+var print = function print(massage) {
+  return console.log(massage);
+};
+
+print('Hi babel!!!');
