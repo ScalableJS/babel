9ef4a1d72c041b7839e820ca2dae95f782576e58
Oleh_Cherkashchenko <oleg056@gmail.com>
2019-06-12 18:10:23 +0300

    test

--- es5.js
+++ es5.js
@@ -1,13 +1,7 @@
 'use strict';
 
-// npm init
-// npm install -g babel-cli --save-dev
-// npm install babel-preset-es2015 --save-dev
-// babel es2015.js --out-file es5.js
-// babel es2015.js -o es5.js --presets es2015
 var print = function print(massage) {
   return console.log(massage);
 };
-var a = '';
-
+var a = 'a';
 print('Hi babel!!!');
