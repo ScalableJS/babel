6dbff173dd75c478fad83b385d17a5582db64e2e
Oleh_Cherkashchenko <oleg056@gmail.com>
2019-06-13 15:47:00 +0300

    test babel

---
+++ dist/es2015.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var print = function print(massage) {
+  return console.log(massage);
+};
+var a = 'a' + (1 + 2);
+print('Hi babel!!!');
---
+++ src/es2015.js
@@ -0,0 +1,3 @@
+const print = massage => console.log(massage);
+const a = `a${1+2}`;
+print('Hi babel!!!');
